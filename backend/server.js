a7f5bf14354c3225e06fb029bacd22d6ce8253bf
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2025-02-02 19:53:39 -0500

    connecting to db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config()
 const addExpenseRoutes=require('./routes/addExpense')
+const mongoose = require('mongoose')
 
 //express app
 const app = express();
@@ -10,12 +11,20 @@ app.use(express.json())
 
 //middleware to console  http's request path and method
 app.use((req,res,next)=>{
-    console.log(req.path, req.mathod)
+    console.log(req.path, req.method)
 })
 
 //routes
 app.use('/api/addExpense',addExpenseRoutes)
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is listening in port: ${port}!`)
-})
+//connect to db and listen to requuest
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server is connected to DB and Listening in port: ${process.env.PORT}!`)
+        })
+    })
+    //else throw error
+    .catch((error)=>{
+        console.log(error)
+    })
