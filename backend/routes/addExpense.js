f4fc4acdac7e6afe02911780ffae665acc03784b
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2025-02-02 19:39:17 -0500

    routes setup

---
+++ backend/routes/addExpense.js
@@ -0,0 +1,19 @@
+//imports
+const express = require('express')
+
+//router import
+const router= express.Router()
+
+//get all expenses
+router.get('/')
+
+//post a single expense
+router.post('/')
+
+//delete a expense
+router.delete('/:id')
+
+// update a expense
+router.patch('/')
+
+module.express=router
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+require('dotenv').config()
+const addExpenseRoutes=require('./routes/addExpense')
 
+//express app
 const app = express();
 
-const port=8000;
-app.listen(port,()=>{
+//to attach json to req body
+app.use(express.json())
+
+//middleware to console  http's request path and method
+app.use((req,res,next)=>{
+    console.log(req.path, req.mathod)
+})
+
+//routes
+app.use('/api/addExpense',addExpenseRoutes)
+
+app.listen(process.env.PORT,()=>{
     console.log(`Server is listening in port: ${port}!`)
 })
