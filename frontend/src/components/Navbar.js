eade17c8f1651b7742466164352a455442987bf0
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2025-02-04 02:30:03 -0500

    App Tittle Fixed

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
         <header>
             <div className="container">
                 <Link to="/">
-                    <h1>Fiance App</h1>
+                    <h1>Finance App</h1>
                 </Link>
             </div>
         </header>
