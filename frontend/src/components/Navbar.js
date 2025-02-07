b966aafc9742f4c0296db44b4c20d5704630443f
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2025-02-07 02:47:38 -0500

    Styling Change

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
         <header>
             <div className="container">
                 <Link to="/">
-                    <h1>Finance App</h1>
+                    <h1 className='title'>Finance App</h1>
                 </Link>
             </div>
         </header>
--- frontend/src/components/expenseDetails.js
+++ frontend/src/components/expenseDetails.js
@@ -2,7 +2,7 @@ const expenseDetails = ({ expense }) => {
     return (
         <div className="expense-details">
             <h4>{expense.tittle}</h4>
-            <p><strong>Amount : </strong>{expense.amount}</p>
+            <p><strong>Amount : </strong>${expense.amount}</p>
             <p><strong>Month : </strong>{expense.month}</p>
             <p>{expense.createdAt}</p>
         </div>
